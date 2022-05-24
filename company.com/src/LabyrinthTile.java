515efbedc4afd3a682b9c128d4c1596868bc9c20
VanjaK89 <kavgic.vanja@gmail.com>
2022-05-24 13:09:55 +0200

    added text

--- company.com/src/LabyrinthTile.java
+++ company.com/src/LabyrinthTile.java
@@ -1,2 +1,27 @@
 public class LabyrinthTile {
+
+
+    private Direction entry;
+    private Direction exit;
+
+    public Direction getEntry() {
+        return entry;
+    }
+
+    public Direction getExit() {
+        return exit;
+    }
+
+    public void setEntry(Direction entry) {
+        this.entry = entry;
+    }
+
+    public void setExit(Direction exit) {
+        this.exit = exit;
+    }
+
+    public LabyrinthTile(Direction entry, Direction exit) {
+        this.entry = entry;
+        this.exit = exit;
+    }
 }
